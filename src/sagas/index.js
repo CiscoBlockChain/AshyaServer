dbc8be529b21601ca100e6674a7704b9b6d6028e
vallard <vallard@benincosa.com>
2018-01-23 08:39:02 -0800

    updated to use sana's contract.

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -5,12 +5,115 @@ import * as actions from '../actions'
 
 
 export function* get_addresses() {
-  var w3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'));
+  //var w3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'));
+  var w3 = new Web3(new Web3.providers.HttpProvider('https://kovan.infura.io/1r0bIX2eewb5e9m2WAug'));
+  //var w3 = callWeb3MethodAsPromise( Web3.providers.HttpProvider, 'http://127.0.0.1:8545');
+  // contract addrdss is:  0x52CB8F783468f4e60642aaa15a3466e026A30Eb3
+  // contracdt address:  0x461f7eb8a0ffc0a0098c5eeedf52fa79af725616
+  // 
   if(!w3.isConnected()) {
     return yield put(actions.gotError("not connected to blockchain"))
-  } else {
-    return yield put(actions.gotAddresses(w3.eth.accounts))
+  } 
+
+    var abiArray = [
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "id",
+				"type": "uint16"
+			}
+		],
+		"name": "getItem",
+		"outputs": [
+			{
+				"name": "iname",
+				"type": "string"
+			},
+			{
+				"name": "val",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "countItemList",
+		"outputs": [
+			{
+				"name": "count",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"name": "iid",
+				"type": "uint16"
+			},
+			{
+				"name": "url",
+				"type": "string"
+			},
+			{
+				"name": "val",
+				"type": "string"
+			}
+		],
+		"name": "addItem",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "id",
+				"type": "uint16"
+			}
+		],
+		"name": "removeItem",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	}
+];
+
+  var MyContract = w3.eth.contract(abiArray);
+  var contractAddress = "0x461f7eb8a0ffc0a0098c5eeedf52fa79af725616";
+  var registry = MyContract.at(contractAddress);
+  const numOfElements = registry.countItemList();
+  console.log("number of elements = " + numOfElements)
+  const results  = [];
+  for (let i = 0; i < numOfElements; i++) {
+    const elem = registry.getItem(i);
+    console.log(elem);
+    results.push(elem);
   }
+  console.log("contracdt instances are: " + results)
+  return yield put(actions.gotAddresses(results))
 } 
 
 export function* watchBlockchainRequest() {

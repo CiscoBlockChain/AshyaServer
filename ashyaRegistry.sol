d0a22733d858b73720a0f6c3086bb1a8e119e84d
sanaAB <sanaabohelal@gmail.com>
2018-03-29 13:27:44 +0200

    Update ashyaRegistry.sol

--- ashyaRegistry.sol
+++ ashyaRegistry.sol
@@ -1,27 +1,30 @@
 pragma solidity ^0.4.19;
 
 contract AshyaRegistry {
+    
     struct item {
-        
         string iname;
         string url;
         string location;
-        address Address;
+        address owner;
     }
-
+    
     uint16 itemCount;
-    mapping(uint16 => item) itemList;
-    item[] itemArray;
+    mapping(uint16 => item)public itemList;
 
     function AshyaRegistry() public {
-        log0('hi');
+        item memory newItem = item({iname:"", url:"", location:"",owner: msg.sender});
         itemCount = 0;
     }
 
-    function addItem(string name, string url, string val,address Add) public {        
-        var itemnew = item(name ,url, val,Add);
-        itemList[itemCount++] = itemnew;
-        itemArray.push(itemnew);
+    function addItem(string name, string url, string val,address owner) public {        
+        var itemnew = itemList[itemCount];
+        require(msg.sender == owner);
+        itemnew.iname = name;
+        itemnew.url = url;
+        itemnew.location = val;
+        itemnew.owner = owner;
+        itemCount++;  
     }
 
     function countItemList()public constant returns (uint count) {     
@@ -29,13 +32,14 @@ contract AshyaRegistry {
     }
 
     function removeItem(uint16 id) public {
-        //TODO: make sure only owners and device owners can delete device.
-        delete itemArray[id];
-        itemCount--;
+        if(itemList[id].owner != msg.sender)
+            return;
+        
+        delete itemList[id];
+        itemCount--;  
     }
     
     function getItem(uint16 id)public constant returns (string name, string url, string location, address add) {   
-        return (itemArray[id].iname, itemArray[id].url, itemArray[id].location, itemArray[id].Address);
-    }
+        return (itemList[id].iname, itemList[id].url, itemList[id].location, itemList[id].owner);
+    }    
 }
-

e211a65fbdb7c9d1499325036dc934ade19dd709
sajustin <sajustin@cisco.com>
2018-04-04 17:48:51 +0200

    Updated AshyaRegistry contract with address mapping

--- ashyaRegistry.sol
+++ ashyaRegistry.sol
@@ -1,39 +1,69 @@
 pragma solidity ^0.4.19;
+
 contract AshyaRegistry {
     struct item {
         
         string iname;
-        string url;
         string location;
-        address Address;
+        string url;
+        address ownerAddress;
+        uint index;
     }
 
-    uint16 itemCount;
-    mapping(uint16 => item) itemList;
-    item[] itemArray;
-
+    mapping(address => item) itemList;
+    address[] itemIndex;
+    
+    event LogNewItem (address indexed itemAddress, uint index, string iname, string location, string url, address ownerAddress);
+    event LogDeleteItem(address indexed itemAddress, uint index);
+    
     function AshyaRegistry() public {
-        log0('hi');
-        itemCount = 0;
+        itemIndex.length = 0;
     }
 
-    function addItem(string name, string url, string val,address Add) public {        
-        var itemnew = item(name ,url, val,Add);
-        itemList[itemCount++] = itemnew;
-        itemArray.push(itemnew);
+    function addItem(string name, string location, string url,address itemAddress, address ownerAddress) public  {        
+        //var itemnew = item(name ,url, location, itemAddress);
+        itemList[itemAddress].iname = name;
+        itemList[itemAddress].location = location;
+        itemList[itemAddress].url = url;
+        itemList[itemAddress].ownerAddress = ownerAddress; 
+        itemList[itemAddress].index = itemIndex.push(itemAddress)-1;
+        
+        LogNewItem(
+            itemAddress,
+            itemList[itemAddress].index,
+            name,
+            location,
+            url,
+            ownerAddress);
     }
 
-    function countItemList()public constant returns (uint count) {     
-        return itemCount;
-    }
-
-    function removeItem(uint16 id) public {
-        //TODO: make sure only owners and device owners can delete device.
-        delete itemArray[id];
-        itemCount--;
+    function getItemCount()public constant returns (uint count) {     
+        return itemIndex.length;
     }
     
-    function getItem(uint16 id)public constant returns (string name, string url, string location, address add) {   
-        return (itemArray[id].iname, itemArray[id].url, itemArray[id].location, itemArray[id].Address);
+    function checkOwnership(address itemAddress) public{
+        require(msg.sender == itemList[itemAddress].ownerAddress || msg.sender == address(this));
+    } 
+
+    function removeItem(address itemAddress) public {
+        checkOwnership(itemAddress);
+        uint rowToDelete = itemList[itemAddress].index;
+        address keyToMove = itemIndex[itemIndex.length-1];
+        itemIndex[rowToDelete] = keyToMove;
+        itemList[keyToMove].index = rowToDelete;
+        itemIndex.length--;
+        LogDeleteItem(
+            itemAddress,
+            rowToDelete);
+        
+    }
+    
+    function getItem(address itemAddress)public constant returns (string name, string url, string location, uint index, address ownerAddress) {   
+        return (itemList[itemAddress].iname, itemList[itemAddress].url, itemList[itemAddress].location, itemList[itemAddress].index, itemList[itemAddress].ownerAddress);
+    }
+    
+    function getItemAtIndex(uint index) public constant returns(address itemAddress) {
+        return itemIndex[index];
     }
 }
+

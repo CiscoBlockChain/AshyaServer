eb9e770ecb33f43a15c0dc040fed5348ac220911
vallard <vallard@benincosa.com>
2019-02-06 16:48:43 -0800

    merges and changes

--- src/components/Docs.js
+++ src/components/Docs.js
@@ -7,9 +7,9 @@ const Docs = ({doc}) => (
           <div className="sidebar-sticky">
             <ul className="nav flex-column">
               <li className="nav-item">
-                <a className="nav-link active" href="#">
+                <a className="nav-link active" href="/Introduction">
                   <span data-feather="home"></span>
-                  Dashboard <span className="sr-only">(current)</span>
+                  Introduction <span className="sr-only">(current)</span>
                 </a>
               </li> 
             </ul>
